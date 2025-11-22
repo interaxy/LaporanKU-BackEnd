916cf7cdbf55d44e0274ad0ac32c12a08aa05871
Interaxyns <akhmdkhrlyn.343@gmail.com>
2025-11-22 20:27:52 +0700

    Update checklists.js

--- routes/checklists.js
+++ routes/checklists.js
@@ -2,7 +2,7 @@ import express from 'express';
 import multer from 'multer';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { pool } from '../db.js';
+import { pool } from '../database.js';
 import { authRequired, isAdmin } from '../middleware/auth.js';
 
 const router = express.Router();
