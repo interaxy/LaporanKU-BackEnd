af8a3425184f7b167e004bfa898ab947ef343137
Interaxyns <akhmdkhrlyn.343@gmail.com>
2025-11-22 20:27:19 +0700

    Update issues.js

--- routes/issues.js
+++ routes/issues.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { pool } from '../db.js';
+import { pool } from '../database.js';
 import { authRequired, isAdmin } from '../middleware/auth.js';
 
 const router = express.Router();
