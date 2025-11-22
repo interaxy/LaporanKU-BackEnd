d110f111268a5d91fe80c12c9b6ddd95c392ca40
Interaxyns <akhmdkhrlyn.343@gmail.com>
2025-11-22 20:26:42 +0700

    Update reports.js

--- routes/reports.js
+++ routes/reports.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { pool } from '../db.js';
+import { pool } from '../database.js';
 import { authRequired } from '../middleware/auth.js';
 
 const router = express.Router();
